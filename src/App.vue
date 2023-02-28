31bd34b38c2c3001168e904ce021f496b134f6ba
= <=>
2023-02-28 11:20:19 -0300

    a

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script setup>
+  import { ref, onMounted } from 'vue'
 
+const contador = ref(0)
+
+function incrementarContador() {
+  contador.value++
+}
+function decrementarContador() {
+  contador.value--
+}
+
+onMounted(() => {
+  console.log(`O valor inicial do contador é ${contador.value}.`)
+})
 </script>
 
 <template>
-  
+  <button @click="incrementarContador">Incrementar</button>
+  <button @click="decrementarContador">Decrementar</button>
+  <p>Valor do contador é: {{ contador }}</p>
 </template>
 
-<style scoped>
-
+<style>
+  button {
+  font-weight:bolder;
+  background-color: deeppink;
+}
+body{
+  background-color: black;
+}
+p{
+  color: aliceblue;
+}
 </style>
